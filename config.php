c2b98186de083a346ce12c2fad7a101c6d7a895f
Prof-acc <77473259+KangProf@users.noreply.github.com>
2021-07-04 12:56:29 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ Ini adalah file konfigurasi untuk moz accessID dan secretkey untuk pemindaian tampilan blogger RED HAWK.
+ Jika Anda bukan seorang blogger atau dengan kata lain jika Anda tidak tertarik dengan beberapa info blogger geeky tentang situs target Anda dapat meninggalkan file ini saja.
+
+ - Tapi saya tertarik dan tidak tahu dari mana mendapatkan kunci ini :'( ...
+ -- Kalau begitu, buka dan buat akun di moz.com dan buat banyak kunci Anda di sini: https://moz.com/products/mozscape/access
+
+ HARAP DICATAT: akun gratis memiliki batasan 27.000 panggilan per bulan yang saya yakini lebih dari cukup tetapi jika ini gagal untuk Anda, buat saja akun baru di moz dan dapatkan banyak kunci baru dan ganti yang lama.
+*/
+
+$accessID = "letakkan id akses Anda di antara tanda kutip"; // ini adalah tempat Anda meletakkan ID akses Anda
+$secretKey = "tempelkan secretkey Anda di antara tanda kutip"; // Kunci rahasia Anda ada di sini
+
+?>
